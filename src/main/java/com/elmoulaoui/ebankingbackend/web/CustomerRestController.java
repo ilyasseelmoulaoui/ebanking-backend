ab1246593d7040f8e5fdb96c1b5f5fad6e3b8386
ilyasseelmoulaoui <ilyasseelmoulaoui00@gmail.com>
2022-06-25 20:01:11 +0100

    part 3 : 2:15:00

--- src/main/java/com/elmoulaoui/ebankingbackend/web/CustomerRestController.java
+++ src/main/java/com/elmoulaoui/ebankingbackend/web/CustomerRestController.java
@@ -19,9 +19,15 @@ public class CustomerRestController {
 
     @GetMapping("/customers")
     public List<CustomerDTO> customers(){
-
         return bankAccountService.listCustomer();
     }
+
+
+    @GetMapping("/customers/search")
+    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword){
+        return bankAccountService.searchCustomers("%"+keyword+"%");
+    }
+
 
     @GetMapping("/customers/{id}")
     public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
