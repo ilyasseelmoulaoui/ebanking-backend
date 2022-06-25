9ded6cc201b76cedae3543bc20980ec49532db7d
ilyasseelmoulaoui <ilyasseelmoulaoui00@gmail.com>
2022-06-25 02:09:46 +0100

    part 3 : 1:07:00

--- src/main/java/com/elmoulaoui/ebankingbackend/web/BankAccountRestAPI.java
+++ src/main/java/com/elmoulaoui/ebankingbackend/web/BankAccountRestAPI.java
@@ -6,14 +6,12 @@ import com.elmoulaoui.ebankingbackend.dtos.BankAccountDTO;
 import com.elmoulaoui.ebankingbackend.entities.AccountOperation;
 import com.elmoulaoui.ebankingbackend.exceptions.BankAccountNotFoundException;
 import com.elmoulaoui.ebankingbackend.services.BankAccountService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class BankAccountRestAPI {
     private BankAccountService bankAccountService;
 
